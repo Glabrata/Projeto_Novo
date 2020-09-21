5ec0922b170d20f0262f931bb2a92866a9b3e926
Glabrata <gdcaldana@gmail.com>
2020-09-21 14:50:40 -0300

    Initial commit

---
+++ GabrielPacote/R/scriptPrincipal.R
@@ -0,0 +1,36 @@
+usethis::use_git()
+1
+usethis::use_git_config(
+  user.name = "Glabrata",
+  user.email = "gdcaldana@gmail.com"
+)
+usethis::use_github()
+2
+2
+usethis::browse_github_token()
+
+here::dr_here()
+
+"publishConfig": { "registry": "https://npm.pkg.github.com/" }
+
+a <- 1
+
+usethis::browse_github_token()
+
+usethis::edit_r_environ()
+
+usethis::create_package("GabrielPacote")
+
+colar = function (x,y) {
+  x + y
+}
+
+colar(x = 10, y = 5)
+
+install.packages("devtools")
+devtools::document()
+1
+
+usethis::create_project("")
+
+
